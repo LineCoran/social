f38ddb74ce0e4c41bd5addabfb9c0dfc73c16a28
Alexey <alexey.kuzmichev1999@gmail.com>
2023-09-24 21:41:32 +0300

    feature/add react and css loader

---
+++ src/components/Counter.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './Counter.scss'
+
+export const Counter = () => {
+
+const [count, setCount] = useState(0)
+
+const increment = () => setCount(prev => prev + 1)
+const dincrement = () => setCount(prev => prev - 1)
+
+  return (
+    <div className='counter'>
+        <h1>{count}</h1>
+        <button onClick={increment}>increment</button>
+        <button onClick={dincrement}>dincrement</button>
+    </div>
+  )
+}
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-console.log('Hello world123123423')
